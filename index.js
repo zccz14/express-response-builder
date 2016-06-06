e6430c060aac552868696a7e49892f67b09315be
Heley Chen <zccz14@outlook.com>
2016-06-07 02:41:59 +0800

    0.1.0

---
+++ index.js
@@ -0,0 +1,33 @@
+function Report(code, msg, body) {
+    return {
+        code: code,
+        msg: msg,
+        body: body
+    };
+}
+
+function SuccessReport(body) {
+    return Report(0, 'ok', body || {});
+}
+
+
+function BUGReport(err) {
+    console.log(err);
+    return Report(-1, 'unexpected error', {});
+}
+
+function PermissionDeniedReport() {
+    return Report(403, 'permission denied', {});
+}
+
+function NotFoundReport() {
+    return Report(404, 'not found', {});
+}
+
+module.exports = {
+    Report: Report,
+    BUGReport: BUGReport,
+    SuccessReport: SuccessReport,
+    Password: Password,
+    NotFoundReport: NotFoundReport,
+};
